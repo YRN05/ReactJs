505ca6b7f2519ad231474f61522427f100b3a78f
YRN05 <93474893+YRN05@users.noreply.github.com>
2022-11-14 20:46:34 +0700

    update 2
    Sudah ada function

--- latihan1.js
+++ latihan1.js
@@ -1,18 +1,20 @@
-console.log("Hello World");
-
-for(let i = 1; i <= 15; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
-    }
-
-    else if (i % 5 === 0){
-        console.log("Buzz");
-    }
-
-    else if (i % 3 === 0){
-        console.log("Fizz");
-    }
-    else{
-        console.log(i);
+function proses(){
+    for(let i = 1; i <= 15; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }
+    
+        else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+    
+        else if (i % 3 === 0){
+            console.log("Fizz");
+        }
+        else{
+            console.log(i);
+        }
     }
 }
+console.log("Welcome to the program");
+proses();
