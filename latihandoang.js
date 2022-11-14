a18024218e871144edca8b744331a0caea812a0b
YRN05 <93474893+YRN05@users.noreply.github.com>
2022-11-14 23:29:57 +0700

    Tempat latihan
    Noted: latihan function

---
+++ latihandoang.js
@@ -0,0 +1,10 @@
+function filter(){
+    if(angka % 2 === 0){
+        console.log("Bilangan ini genap");
+    }
+    else{
+        console.log("Angka ini ganjil");
+    } 
+}
+filter();
+let angka = 10;
