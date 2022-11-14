c80e80f5c10edad57c6cf4081d00dd08b709358e
YRN05 <93474893+YRN05@users.noreply.github.com>
2022-11-14 23:29:36 +0700

    Chalange ke 2
    Sudah selesai

---
+++ latihan2.js
@@ -0,0 +1,9 @@
+function proses(){
+    for(let i = input; i >= 1; i--){
+        hasil = i * i-1;
+        console.log(hasil);
+    }
+}
+let input = 10;
+console.log("Program bilangan faktorial");
+proses();
