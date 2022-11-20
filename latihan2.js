88b3a64c9ea0d77627306594a48e227eb86ff044
YRN05 <93474893+YRN05@users.noreply.github.com>
2022-11-20 16:01:36 +0700

    Update latihan2.js
    Masih ada problem

--- latihan2.js
+++ latihan2.js
@@ -1,8 +1,9 @@
+let hasil;
 function proses(){
-    for(let i = input; i >= 1; i--){
-        hasil = i * i-1;
-        console.log(hasil);
+    for(let i = input; i > 1; i--){
+        hasil = hasil * i;
     }
+    console.log(hasil);
 }
 let input = 10;
 console.log("Program bilangan faktorial");
